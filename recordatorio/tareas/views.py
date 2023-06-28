e271fb707a94ddf9f86e0fc34f83b70d43a1b469
MochitoSpooky <119640815+MochitoSpooky@users.noreply.github.com>
2023-06-28 03:18:14 -0400

    staticfiles

--- recordatorio/tareas/views.py
+++ recordatorio/tareas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+
+class LoginView(LoginView):
+    template_name = 'tareas/login.html'  # Ruta al archivo de plantilla de login
+
+
+def base(request):
+    return render(request, 'tareas/base.html')
 
 # Create your views here.
